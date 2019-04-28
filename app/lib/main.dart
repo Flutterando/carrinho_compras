edb5ab112b0bf8a105c9778e59df2555e677ac20
Jacob Moura <tecleigo@gmail.com>
2019-04-27 23:33:26 -0300

    add app module

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,7 @@
-import 'package:app/src/home/HomePage.dart';
+import 'package:app/src/App.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
 
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
 
