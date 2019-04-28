24b6420350b6dd865d626262fa76706ec1845451
Jacob Moura <jacobaraujo7@gmail.com>
2019-04-28 20:10:58 -0300

    adicionado flutter desktop

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,8 +2,24 @@ import 'package:app/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
+import 'dart:io' show Platform;
 
 void main() {
-  // debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  _setTargetPlatformForDesktop();
   runApp(App());
+}
+
+/// If the current platform is desktop, override the default platform to
+/// a supported platform (iOS for macOS, Android for Linux and Windows).
+/// Otherwise, do nothing.
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
 }
