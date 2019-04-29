0680f2d5fad8300d15150a74ba2446692fdb9068
Jacob Moura <tecleigo@gmail.com>
2019-04-29 20:48:00 -0300

    botão para voltar a tela inicial limpar lista

--- app/lib/src/finalizar_pedido/finalizar_pedido_page.dart
+++ app/lib/src/finalizar_pedido/finalizar_pedido_page.dart
@@ -1,6 +1,9 @@
+import 'package:app/src/carrinho/carrinho_bloc.dart';
 import 'package:app/src/finalizar_pedido/componentes/progressbar_widget.dart';
 import 'package:app/src/finalizar_pedido/componentes/texto_widget.dart';
 import 'package:app/src/finalizar_pedido/finalizar_pedido_bloc.dart';
+import 'package:app/src/produtos/produtos_page.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 
 class FinalizarPedidoPage extends StatefulWidget {
@@ -12,34 +15,45 @@ class _FinalizarPedidoPageState extends State<FinalizarPedidoPage> {
   FinalizarPedidoBloc bloc;
 
   Widget _msgSucesso() {
-    return Container(
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextoWidget(
-                texto: 'Parabéns!! \n seu pedido foi realizado',
-                fontSize: 25,
-                color: Color.fromRGBO(27, 201, 168, 1),
-              ),
-              TextoWidget(
-                texto: 'Nº do Pedido: 123',
-                fontSize: 20,
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        BlocProviderList.of<CarrinhoBloc>(context).trucateList();
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProdutosPage(),
           ),
-        ));
+        );
+      },
+      child: Container(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextoWidget(
+                  texto: 'Parabéns!! \n seu pedido foi realizado',
+                  fontSize: 25,
+                  color: Color.fromRGBO(27, 201, 168, 1),
+                ),
+                TextoWidget(
+                  texto: 'Nº do Pedido: 123',
+                  fontSize: 20,
+                ),
+              ],
+            ),
+          )),
+    );
   }
 
   @override
   void initState() {
     super.initState();
     bloc = FinalizarPedidoBloc();
-    
-    // --  
-    Future.delayed(Duration(seconds: 4)).then((_){
-        bloc?.loadingEvent?.add(false);
+
+    // --
+    Future.delayed(Duration(seconds: 4)).then((_) {
+      bloc?.loadingEvent?.add(false);
     });
   }
 
@@ -47,10 +61,6 @@ class _FinalizarPedidoPageState extends State<FinalizarPedidoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-          title: Text("Finalizar Pedido"),
-          centerTitle: true,
-        ),
         body: StreamBuilder<bool>(
             stream: bloc?.loadingFlux,
             initialData: true,
@@ -63,7 +73,6 @@ class _FinalizarPedidoPageState extends State<FinalizarPedidoPage> {
                 widgetSelecionado = _msgSucesso();
               }
               return widgetSelecionado;
-            })
-        );
+            }));
   }
 }
